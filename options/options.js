7280977c24cf6616b58a72d7520473bfc463552e
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 07:42:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "stangingdata-dev-94105",
+  url: "https://stangingdata-dev-94105.botics.co",
+  api: "https://stangingdata-dev-94105.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
